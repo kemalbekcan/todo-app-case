d803c6f5bfc5fffdff8efb3ead1b9005c3e3b5f2
kemalbekcan <kemalbekcan@gmail.com>
2024-06-10 18:13:26 +0300

    initial commit

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -4,6 +4,7 @@ const accessTokenSecret = "yourSecretKey";
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
+  console.log('tokennn', token)
 
   if (!token) {
     return res.sendStatus(401); // Unauthorized
